c27df630ea243c451e5f3e3bd0e9f25c0aea826b
Francisco Perez-Sorrosal <fperezsorrosal@gmail.com>
2019-04-20 12:50:31 -0700

    Differentiate regular and extra frames

--- src/bowling_kata/bowling.py
+++ src/bowling_kata/bowling.py
@@ -1,4 +1,5 @@
 # TODO: Refactor this shit
+from abc import abstractmethod
 from enum import Enum
 
 
@@ -17,27 +18,42 @@ def get_points_per_roll(roll_char, previous_roll_points=0):
         return RollOutput.SPARE, 10 - previous_roll_points
 
 
-class Frame:
+class BaseFrame:
 
-    def __init__(self, frame_str, is_extra=False):
+    def __init__(self, frame_str):
         self.frame_str = frame_str
-        self.is_extra = is_extra
         self.rolls = [0, 0]
         self.strike = False
         self.spare = False
 
+    @abstractmethod
     def calculate_points(self):
-        points = 0
-        for i, char in enumerate(self.frame_str):
+        pass
 
+
+class RegularFrame(BaseFrame):
+
+    def __init__(self, frame_str):
+        super().__init__(frame_str)
+
+    def calculate_points(self):
+        for i, char in enumerate(self.frame_str):
             roll_output, self.rolls[i] = get_points_per_roll(char) if i == 0 else get_points_per_roll(char, self.rolls[i - 1])
-            if not self.is_extra:
-                if roll_output == RollOutput.STRIKE:
-                    self.strike = True
-                if roll_output == RollOutput.SPARE:
-                    self.spare = True
-        if self.is_extra:
-            print(self.rolls)
+            if roll_output == RollOutput.STRIKE:
+                self.strike = True
+            if roll_output == RollOutput.SPARE:
+                self.spare = True
+        return sum(self.rolls)
+
+
+class ExtraFrame(BaseFrame):
+
+    def __init__(self, frame_str):
+        super().__init__(frame_str)
+
+    def calculate_points(self):
+        for i, char in enumerate(self.frame_str):
+            roll_output, self.rolls[i] = get_points_per_roll(char) if i == 0 else get_points_per_roll(char, self.rolls[i - 1])
         return sum(self.rolls)
 
 
@@ -51,7 +67,7 @@ class Bowling:
     def calculate_frames_points(self, frames_str, extra_frame_str):
         frames_points = 0
         frames = self.split_frames(frames_str)
-        frames.append(Frame(extra_frame_str, True))
+        frames.append(ExtraFrame(extra_frame_str))
         print(len(frames))
         for i in range(10):
             print(i)
@@ -65,8 +81,8 @@ class Bowling:
                 if frames[i + 1].strike and i + 2 < len(frames):
                     frames[i + 2].calculate_points()
                     frames_points += frames[i + 2].rolls[0]
-                if i == 9:
-                    print("a {} {}".format(frames[i + 1].calculate_points(), frames[i + 1].is_extra))
+                # if i == 9:
+                #     print("a {} {}".format(frames[i + 1].calculate_points(), frames[i + 1].is_extra))
         return frames_points
 
     def split_input(self, game_str):
@@ -76,6 +92,6 @@ class Bowling:
         frames_strs = frames_str.split('|')
         frames = []
         for frame_str in frames_strs:
-            frames.append(Frame(frame_str))
+            frames.append(RegularFrame(frame_str))
         assert len(frames) == 10
         return frames
